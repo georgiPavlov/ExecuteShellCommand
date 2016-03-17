542dd504a33d3f1e57e85eb2321da83cd2cc44ac
georgiPavlov <georgi95.bg@gmail.com>
2016-03-17 02:55:05 +0200

    update

--- src/Runner.java
+++ src/Runner.java
@@ -6,10 +6,15 @@ public class Runner {
         long waitTime = 1000;
         int howMany = 3;
         long howManyMinutes = 6000;
-       // Thread runnable = new Thread(new ExecuteShellCommandEAPOL("sudo mdk3",System.currentTimeMillis(),waitTime));
-        //runnable.start();
+        Thread runnable =
+                new Thread(new ExecuteShellCommandEAPOL(
+                        "sudo mdk3",System.currentTimeMillis(),waitTime,howManyMinutes)
+                );
+        runnable.start();
         for (int i = 0; i < howMany ; i++) {
-            new Thread(new ExecuteShellCommandDDOS("sudo mdk3",System.currentTimeMillis(),waitTime,howManyMinutes)).start();
+            new Thread(
+                    new ExecuteShellCommandDDOS("sudo mdk3",System.currentTimeMillis(),waitTime,howManyMinutes)
+            ).start();
         }
     }
 }
